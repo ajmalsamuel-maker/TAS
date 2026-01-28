9c9230ec681c8957ae7827bea1de2dff7af3e12e
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2026-01-28 14:42:59 +0000

    File changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { createPageUrl } from '../utils';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
