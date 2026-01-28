4a54682d2bf865e5393bad00c8db66b2bcad095f
Ajmal Victor Samuel <ajmalsamuel@gmail.com>
2026-01-28 07:58:39 +0000

    File changes

--- src/pages/CSPSolution.jsx
+++ src/pages/CSPSolution.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { createPageUrl } from './utils';
+import { createPageUrl } from '../utils';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
